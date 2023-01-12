d8635fc51ff4f5390a73dad36fb2eef7e75a4f14
romealnnr <romeal.nansingaham@gmail.com>
2023-01-12 09:19:26 +0100

    shipping Cart updated

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
@@ -8,7 +8,7 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class CheckoutComponent implements OnInit{
 
-  checkoutFormGroup: FormGroup | undefined; //set of forms...
+  checkoutFormGroup!: FormGroup; //set of formcontrols or other groups...
   
   constructor(private formBuilder: FormBuilder){}
  
@@ -18,7 +18,45 @@ export class CheckoutComponent implements OnInit{
         firstName: [''],
         lastName: [''],
         email: [''],
-      })
-    });
+      }),
+      shippingAddress:  this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: [''],
+      }),
+      billingAddress:  this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: [''],
+      }),
+      creditCard:  this.formBuilder.group({
+        cardType: [''],
+        nameOnCard: [''],
+        cardNumber: [''],
+        securityCode: [''],
+        expirationMonth: [''],
+        expirationYear: ['']
+      }),
+    })
   }
+
+  copyShippingToBillingAdress(event: any){
+    if(event.target.checked){
+      this.checkoutFormGroup.controls['billingAddress'].setValue(this.checkoutFormGroup.controls['shippingAddress'].value);
+    }
+    else{
+      this.checkoutFormGroup.controls['billingAddress'].reset();
+    }
+  }
+  onSubmit() {
+    console.log("Handling the submit button");
+    console.log(this.checkoutFormGroup.get('customer')?.value);
+    console.log(`The email address is ${this.checkoutFormGroup.get('customer')?.value.email}`);
+  }
+
+ 
 }
